de3ac1f6477b91af5d767022000ebdbeb4e559b5
Dionatan wiggers <dionegalato1212@gmail.com>
2025-08-20 18:13:00 -0300

    Atualizar o sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,38 @@
-self.addEventListener("install", e => {
+const CACHE_NAME = "gigas-cache-v1";
+const urlsToCache = [
+  "index.html",
+  "manifest.json",
+  "IMG_6136.png",
+  "IMG_6138.png",
+  "IMG_6139.png",
+  "IMG_6144.ico"
+];
+
+// Instala e faz cache inicial
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
   console.log("Service Worker instalado");
 });
 
-self.addEventListener("fetch", e => {
-  // Futuro: cache offline
+// Ativa e remove caches antigos
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+  console.log("Service Worker ativado");
+});
+
+// Intercepta requisições e serve do cache
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
