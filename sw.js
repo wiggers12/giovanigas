c0727aca82af32a0522bd26e421b11a39620afdd
Dionatan wiggers <dionegalato1212@gmail.com>
2025-08-20 19:58:35 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,8 @@
 self.addEventListener("install", e=>{
-  e.waitUntil(
-    caches.open("gigas-store").then(cache=>{
-      return cache.addAll([
-        "/",
-        "/index.html",
-        "/manifest.json",
-        "/favicon-32.png",
-        "/favicon-192.png",
-        "/favicon-512.png"
-      ]);
-    })
-  );
+  e.waitUntil(caches.open("gigas-cache").then(cache=>{
+    return cache.addAll(["/", "/index.html", "/favicon-192.png"]);
+  }));
 });
-
 self.addEventListener("fetch", e=>{
   e.respondWith(
     caches.match(e.request).then(resp=>resp || fetch(e.request))
