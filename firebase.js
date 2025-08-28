1d23f03bb808f045f42bf11a6436960ae4813818
Dionatan wiggers <dionegalato1212@gmail.com>
2025-08-28 08:46:54 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,28 +1,34 @@
 // firebase.js
-// Importa as funções necessárias do SDK do Firebase
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-analytics.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-storage.js";
+// Este é o seu arquivo central de configuração do Firebase
 
-// Configuração ÚNICA do Firebase para o seu projeto
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+
+// SUAS CREDENCIAIS DO FIREBASE
 const firebaseConfig = {
-  apiKey: "AIzaSyC4h0J54TZqT8XXUsPSL3_ABVJQ05yP4Hc",
-  authDomain: "giovanigas-d959b.firebaseapp.com",
-  projectId: "giovanigas-d959b",
-  storageBucket: "giovanigas-d959b.firebasestorage.app",
-  messagingSenderId: "728326355548",
-  appId: "1:728326355548:web:9dda06e1c1148337618499",
-  measurementId: "G-ESHC9P6XGZ"
+    apiKey: "AIzaSyC4h0J54TZqT8XXUsPSL3_ABVJQ05yP4Hc",
+    authDomain: "giovanigas-d959b.firebaseapp.com",
+    projectId: "giovanigas-d959b",
+    storageBucket: "giovanigas-d959b.firebasestorage.app",
+    messagingSenderId: "728326355548",
+    appId: "1:728326355548:web:9dda06e1c1148337618499",
+    measurementId: "G-ESHC9P6XGZ"
 };
 
-// Inicializa o Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const auth = getAuth(app);
-const db = getFirestore(app);
-const storage = getStorage(app);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
 
-// Exporta as instâncias para que outros arquivos possam usá-las
-export { app, analytics, auth, db, storage };
+// A função que verifica a autenticação, agora no seu arquivo firebase.js
+export function checkUserAuth(callback) {
+    onAuthStateChanged(auth, (user) => {
+        if (user) {
+            // Se o usuário estiver logado, executa o callback
+            callback(user);
+        } else {
+            // Se não, redireciona para a página de login
+            window.location.href = 'index.html';
+        }
+    });
+}
